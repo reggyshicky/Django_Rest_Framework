dc68d40f343285582709f6fef59510b2183befac
reggyshicky <reginahshikanda@gmail.com>
2023-10-24 12:27:24 +0300

    working api to retrieve list of data

---
+++ Drinks/drinks/drinks/views.py
@@ -0,0 +1,13 @@
+#endpoints
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+
+
+def drink_list(request):
+    #get all the drinks, serialize them
+    #return json
+    drinks = Drink.objects.all()
+    serializer = DrinkSerializer(drinks, many=True) #serializes all of them
+    return JsonResponse({'drinks': serializer.data}, safe=False)
+    
